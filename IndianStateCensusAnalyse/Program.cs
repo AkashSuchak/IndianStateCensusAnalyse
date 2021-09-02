07074c1d9295b9ebdb9049966f8ae0a75413a5f9
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-09-02 17:53:48 +0530

    Add project files.

---
+++ IndianStateCensusAnalyse/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("::: Welcome To Indian State Census Analyse :::");
+            Console.WriteLine("----------------------------------------------");
+        }
+    }
+}
